9520dc2c2db0db416dd12e5b5eed459521521e15
manuel <manuel@Gondolyn>
2013-07-07 01:48:23 +0200

    Algorithmus überlegt und das Zerlegen der Zahl entworfen

--- test/katas/FroehlicheZahlenTest.java
+++ test/katas/FroehlicheZahlenTest.java
@@ -6,23 +6,39 @@ package katas;
 
  Beispiel:
  19 -> 1^2 + 9^2 = 82
-    -> 8^2 + 2^2 = 68
-    -> 6^2 + 8^2 = 100
-    -> 1^2 + 0^2 + 0^2 = 1
+ -> 8^2 + 2^2 = 68
+ -> 6^2 + 8^2 = 100
+ -> 1^2 + 0^2 + 0^2 = 1
  */
 
-import static org.junit.Assert.*;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class FroehlicheZahlenTest {
 
   @Test
   public void testIstFroehlicheZahl() {
-    assertEquals(true,istFroehlicheZahl(19));
+    assertEquals(true, istFroehlicheZahl(1));
   }
 
-  public boolean istFroehlicheZahl(int zahl){
-    return true;
+  // Integrationstest
+  public boolean istFroehlicheZahl(int zahl) {
+    return (zahl == 1) ? true : false;
+    // quadriere jede Ziffer
+    // summiere die Quadrate
+    // entscheide, ob die Zahl wieder zerlegt werden kann oder
+    // entscheide, ob die Summe das Kriterium fröhliche Zahl erfüllt
+  }
+
+  @Test
+  public void zerlegeZahlTest() {
+    assertEquals(new Integer[] { 1, 1 }, zerlegeZahl(11));
+  }
+
+    // zerlege die Zahl in Ziffern
+  public Integer[] zerlegeZahl(int zahl) {
+    return new Integer[] { 1, 1 };
   }
 
 }
