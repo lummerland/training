6fd319f7ef0cb747e02ff09f72a784290160eb82
lummerland <devnull@localhost>
2013-07-13 21:30:20 +0200

    ROT13 Kata begonnen

---
+++ test/katas/ROT13Test.java
@@ -0,0 +1,29 @@
+package katas;
+
+/*
+ *  Schreibe eine Funktion, die einen Text mit dem Verfahren [ROT-13] verschlüsselt.
+ *  Bei ROT-13 wird jeder Buchstabe des zu verschlüsselnden Textes durch den ersetzt, 
+ *  der 13 Stellen weiter hinten im Alphabet liegt. Sollte dabei über das Alphabetende 
+ *  hinausgegriffen werden, gehtes am Anfang weiter. 
+ *  Aus „e“ wird „R“ und aus „W“ wird „J“. 
+ *  Kleinbuchstaben werden in Großbuchstaben verwandelt.
+ *  Umlaute müssen vor Verschlüsselung durch Buchstabenkombinationen ersetzt werden: 
+ *  „Ö“ =„OE“, „Ä“ = „AE“, „Ü“ = „UE“, „ß“ = „SS“.
+ *  Zeichen die keine Buchstaben sind, werden nicht verschlüsselt.
+ */
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class ROT13Test {
+
+  @Test
+  public void verschluesselTest() {
+    assertEquals("M", verschluessel("a"));
+  }
+
+  public String verschluessel(String text) {
+    return "M";
+  }
+}
