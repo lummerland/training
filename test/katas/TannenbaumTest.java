6e68d62c63d5390c57ca6c48daf17ee29e705b86
lummerland <devnull@localhost>
2013-08-01 16:16:14 +0200

    Algorithmus überlegt und Infos zum Verständnis der Aufgabe notiert

--- test/katas/TannenbaumTest.java
+++ test/katas/TannenbaumTest.java
@@ -11,6 +11,21 @@ import org.junit.Test;
 // xxxxxxx
 //    x
 
+// diverse Infos:
+// jede Reihe hat zwei x-Zeichen mehr als die darüber liegende
+// die Menge der x-Zeichen in einer Reihe ist immer ungerade
+// die Leerzeichen werden durch Spaces erzeugt
+// jede Reihe hat rechts und links die gleiche Menge an Leerzeichen
+// jede Reihe ist durch einen Umbruch von der nächsten getrennt
+// die Summe aller Zeichen ist in jeder Reihe gleich und entspricht der Zeichenmenge der untersten Reihe
+// ganz unten kommt immer ein Fuß, der zählt aber nicht zur Höhe und ist eine Kopie der Spitze
+
+// Ablauf:
+// die erste Reihe besteht aus 	(Höhe - 1) * Leerzeichen . x . (Höhe - 1) * Leerzeichen
+// die zweite Reihe besteht aus (Höhe - 2) * Leerzeichen . x . (Höhe - 2) * Leerzeichen
+// die n-te Reihe besteht aus 	(Höhe - n) * Leerzeichen . x . (Höhe - n) * Leerzeichen
+// wenn Fertig, dann der Fuß		(Höhe - 1) * Leerzeichen . x . (Höhe - 1) * Leerzeichen
+
 public class TannenbaumTest {
 
 	@Test
